28b44c23c874d57542f479505c43d60c4e35cb9e
jzhou <zhou0309@umn.edu>
2019-08-20 23:21:00 -0500

    add alt, add src, add login directory

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-/**
- * @format
- */
+import {
+  AppRegistry,
+  StatusBar
+} from 'react-native';
 
-import {AppRegistry} from 'react-native';
 import App from './App';
+
 import {name as appName} from './app.json';
+
+StatusBar.setBarStyle('light-content', true)
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/views/login/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Text,
+  TouchableHighlight,
+  StyleSheet,
+  View
+} from 'react-native';
+
+const styles = StyleSheet.create({
+
+})
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillUnMount() {
+
+  }
+
+
+}
+
+export default LoginView
