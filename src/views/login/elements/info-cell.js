8a64eddfa3e1b1d4975f8dd31158a77e2eb7d7c6
jzhou <zhou0309@umn.edu>
2019-08-22 18:42:49 -0500

    add info-cell, add login view

---
+++ src/views/login/elements/info-cell.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 40,
+    backgroundColor: 'white'
+  },
+  infoTextInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderBottomWidth: 1,
+    padding: 5,
+    color: 'black',
+  }
+})
+
+class InfoCell extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hint: props.hint
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.infoTextInput}
+          onChangeText={this.props.onChangeText}
+          placeholder={this.state.hint}
+          autoCapitalize={'none'}
+        />
+      </View>
+  )}
+}
+
+export default InfoCell;
--- src/views/login/index.js
+++ src/views/login/index.js
@@ -1,19 +1,74 @@
 import React from 'react';
 import {
-  Text,
   TouchableHighlight,
+  Keyboard,
+  Platform,
+  Text,
+  SafeAreaView,
   StyleSheet,
   View
 } from 'react-native';
 
-const styles = StyleSheet.create({
+// View Elements
+import InfoCell from './elements/info-cell';
 
+const styles = StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 23,
+    fontWeight: 'bold',
+  },
+  headerView: {
+    flex:1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  bottomView: {
+    flex:3,
+  },
+  statusBar: {
+    height: 24,
+  },
+  settings: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  signInButton: {
+    backgroundColor: '#24A0ED',
+    borderRadius: 10,
+    height: 40,
+    width: 100,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  signInText: {
+    color: 'white',
+    fontWeight: 'bold'
+  }
 })
 
-class Login extends React.Component {
+class LoginView extends React.Component {
   constructor(props) {
     super(props)
-    
+    this.state = {
+      email: "",
+      password: ""
+    }
+    this.onChangeEmail = this.onChangeEmail.bind(this)
+    this.onChangePassword = this.onChangePassword.bind(this)
   }
 
   componentDidMount() {
@@ -24,7 +79,51 @@ class Login extends React.Component {
 
   }
 
+  onChangeEmail(text) {
+    this.setState({
+      email: text
+    })
+  }
 
+  onChangePassword(text) {
+    this.setState({
+      password: text
+    })
+  }
+
+  onPressSignIn() {
+    console.log(this.state.email)
+    console.log(this.state.password)
+    Keyboard.dismiss()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {Platform.OS === 'ios' && <View style={styles.statusBar} />}
+        <SafeAreaView style={styles.safeAreaView}>
+          <View style={styles.headerView}>
+            <Text style={styles.headerText}>A Message Project</Text>
+          </View>
+          <View style={{flex:1.5}}>
+            <InfoCell hint={"Email"} onChangeText={this.onChangeEmail}/>
+            <InfoCell hint={"Password"} onChangeText={this.onChangePassword}/>
+          </View>
+          <View style={{alignItems: 'center'}}>
+            <TouchableHighlight
+              style={styles.signInButton}
+              onPress={this.onPressSignIn.bind(this)}
+            >
+              <Text style={styles.signInText}>
+                Sign In
+              </Text>
+            </TouchableHighlight>
+          </View>
+          <View style={styles.bottomView} />
+        </SafeAreaView>
+      </View>
+    );
+  }
 }
 
 export default LoginView
